4c9b0923834a57c854c98ad0f63ffe92bf69c73e
Narendra Babu <netnaren@gmail.com>
2023-09-28 21:29:32 -0700

    Updates

---
+++ VidhyaApi/Controllers/MathController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VidhyaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        // GET: api/<MathController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MathController>/5
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            return id * id;
+        }
+
+        // POST api/<MathController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MathController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MathController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
